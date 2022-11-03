1d231b4fbec166fae8654d88dd2922b0793e835f
lyesb <lyes.b@createam-dz.com>
2022-11-03 11:16:45 +0100

    Added resize image from PIL

--- screenshot.py
+++ screenshot.py
@@ -1,5 +1,5 @@
 import configparser
-
+from PIL import Image
 from mss import mss
 
 config = configparser.ConfigParser()
@@ -11,5 +11,11 @@ monitor = config.getint("input", "monitor")
 def take_screenshot():
     with mss() as sct:
         sct.shot(mon=monitor)
+        
+    #resize the screenshot
+    image = Image.open(f"monitor-{monitor}.png")
+    image.thumbnail((1920,1080), Image.ANTIALIAS)
+    image.save(f"monitor-{monitor}.png","png")
+
     print("*snap*")
     return f"monitor-{monitor}.png"
